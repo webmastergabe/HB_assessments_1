bf0771cdaffdf800aca71f09f7177fd0a392b957
Teo Vortex <vortexteo@gmail.com>
2023-12-01 16:01:56 -0600

    Started Assessment 1

---
+++ hello.js
@@ -0,0 +1,6 @@
+/* 
+You don't need to make any changes to this file - it's just here so that
+you can run it to verify you've properly downloaded all the materials
+*/
+
+console.log("You're ready to begin!");
---
+++ variables.js
@@ -0,0 +1,45 @@
+//////////////////PROBLEM 1////////////////////
+/* Create a variable called 'myName' with a value that is your name as a string. */
+
+const myName = 'Gabriel';
+console.log(myName);
+//CODE HERE 
+
+//////////////////PROBLEM 2////////////////////
+/* Create a variable called 'faveNum' with a value that is your favorite number. */
+
+const faveNum = 369
+console.log(faveNum);
+//CODE HERE
+
+//////////////////PROBLEM 3////////////////////
+
+/* Create a variable called 'lovesCode' and assign it a boolean value. */
+
+let lovesCode = true
+console.log(lovesCode);
+//CODE HERE
+
+//////////////////PROBLEM 4////////////////////
+/*
+  Write an if-statement that checks if the faveNum variable is 
+  equal to 13. If it is, console.log('lucky 13!'). 
+  If it's not, console.log('not lucky 13').
+*/
+if (faveNum === 13) {
+  console.log('lucky 13!');
+}
+else {
+  console.log('not lucky 13');
+}
+//CODE HERE
+
+//////////////////PROBLEM 5////////////////////
+/*
+  Write a for loop that prints out the numbers from 1 through 5.
+*/
+
+for (let i = 0; i <= 5; i++) {
+  console.log(i);
+}
+//CODE HERE
